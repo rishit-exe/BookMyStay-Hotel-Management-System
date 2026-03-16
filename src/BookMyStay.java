6e67effa545cabe43c476cd6ad0cc2b78a7766c0
rishit-exe <rishit.vns05@gmail.com>
2026-03-16 08:59:36 +0530

    UC7: Add-On Service Selection

--- src/BookMyStay.java
+++ src/BookMyStay.java
@@ -156,6 +156,59 @@ class RoomAllocationService {
     }
 }
 
+class Service {
+
+    private String serviceName;
+    private double cost;
+
+    public Service(String serviceName, double cost) {
+        this.serviceName = serviceName;
+        this.cost = cost;
+    }
+
+    public String getServiceName() {
+        return serviceName;
+    }
+
+    public double getCost() {
+        return cost;
+    }
+}
+
+class AddOnServiceManager {
+
+    private Map<String, List<Service>> servicesByReservation;
+
+    public AddOnServiceManager() {
+        servicesByReservation = new HashMap<>();
+    }
+
+    public void addService(String reservationId, Service service) {
+
+        if (!servicesByReservation.containsKey(reservationId)) {
+            servicesByReservation.put(reservationId, new ArrayList<>());
+        }
+
+        servicesByReservation.get(reservationId).add(service);
+    }
+
+    public double calculateTotalServiceCost(String reservationId) {
+
+        double total = 0.0;
+
+        if (!servicesByReservation.containsKey(reservationId)) {
+            return total;
+        }
+
+        for (Service service : servicesByReservation.get(reservationId)) {
+            total += service.getCost();
+        }
+
+        return total;
+    }
+}
+
+
 public class BookMyStay {
 
     public static void main(String[] args) {
@@ -182,5 +235,20 @@ public class BookMyStay {
 
             allocationService.allocateRoom(reservation, inventory);
         }
+
+        System.out.println("\nAdd-On Service Selection");
+
+        AddOnServiceManager serviceManager = new AddOnServiceManager();
+
+        Service spa = new Service("Spa", 1500.0);
+
+        String reservationId = "Single-1";
+
+        serviceManager.addService(reservationId, spa);
+
+        double totalCost = serviceManager.calculateTotalServiceCost(reservationId);
+
+        System.out.println("Reservation ID: " + reservationId);
+        System.out.println("Total Add-On Cost: " + totalCost);
     }
 }
